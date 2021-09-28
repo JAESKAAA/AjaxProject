d66a7953605ee9dcee3cb045e12d0a871a25f7be
JAESKAAA <jaeskaaa@gmail.com>
2021-09-28 09:26:20 +0900

    1st Ajax project

---
+++ src/com/ajaxstudy/contact/util/Converter.java
@@ -0,0 +1,38 @@
+package com.ajaxstudy.contact.util;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+//자바 <-> JSON 상호 변환하게 해주는 라이브러리
+import com.google.gson.Gson;
+
+public class Converter {
+	private static Gson gson;
+	
+	//static block gson 객체 초기화
+	static {
+		gson = new Gson();
+	}
+	
+	// JAVA 객체 -> JSON 문자열로 변환
+	public static String convertToJson(Object obj) {
+		return gson.toJson(obj);
+	}
+	//Json 문자열 -> (해당 클래스타입의)Java 객체로 변환
+	public static <T> T convertFromJson(String json, Class<T> type) {
+		return gson.fromJson(json, type);
+	}
+	
+	//브라우저와 연결된 스트림에서 전송된 Json 문자열 -> (해당 클래스타입의)Java 객체로 변환
+	public static <T> T convertFromJsonStream(InputStream is, Class<T> type ){
+		try {
+			Reader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			return gson.fromJson(reader, type);
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
